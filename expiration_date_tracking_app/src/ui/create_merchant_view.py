01b00e9a07d095dfef72cda9c35368e635fa032f
Hanna Jantunen <hannajant@gmail.com>
2026-04-07 11:12:56 +0300

    rekisteröitymisnäkymän back-nappula

--- expiration_date_tracking_app/src/ui/create_merchant_view.py
+++ expiration_date_tracking_app/src/ui/create_merchant_view.py
@@ -38,9 +38,14 @@ class CreateMerchantView:
         except ValueError as error:
             self._error_message.config(text=f"Error: {error}")
 
+    def _handle_back(self):
+        self._handle_show_login_view()
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        title = ttk.Label(master=self._frame, text="Register as merchant")
+        title = ttk.Label(master=self._frame, text="Register as merchant", font=(None, 24, "bold"))
+
+        back_button = ttk.Button(master=self._frame, text="Back", command=self._handle_back)
 
         label_username = ttk.Label(master=self._frame, text="Username")
         self._entry_username = ttk.Entry(master=self._frame)
@@ -60,6 +65,7 @@ class CreateMerchantView:
         )
 
         title.grid(row=0, column=0, columnspan=2, pady=5)
+        back_button.grid(row=0, column=2, sticky=constants.NE, pady=(0, 10))
         label_username.grid(row=1, column=0, padx=5, pady=5)
         self._entry_username.grid(row=1, column=1, padx=5, pady=5)
         label_password.grid(row=2, column=0, padx=5, pady=5)
